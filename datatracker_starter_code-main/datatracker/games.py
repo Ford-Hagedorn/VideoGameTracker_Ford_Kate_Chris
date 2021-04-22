3e514f93b5febc1878887dec848c2d0324209783
katejoyreed <katejoyreed@gmail.com>
2021-04-22 11:24:05 -0500

    Added provisional chart to html file

--- datatracker_starter_code-main/datatracker/games.py
+++ datatracker_starter_code-main/datatracker/games.py
@@ -21,49 +21,8 @@ def search_by_name(input):
         if game.name == input:
             return game
 
-def search_by_year(input):
+@bp.route('/Console')
+def view_consoles():
     games = index()
     for game in games:
-        if game.year == input:
-            return game
-
-def search_by_console(input):
-    games = index()
-    for game in games:
-        if game.platform == input:
-            return game
-
-def search_by_genre(input):
-    games = index()
-    for game in games:
-        if game.genre == input:
-            return game
-
-from flask.views import View
-
-class ShowGames(View):
-
-    def dispatch_request(self):
-
-        pass
-
-
-class ListView(View):
-
-    def get_template_name(self):
-        raise NotImplementedError()
-
-    def render_template(self, context):
-        return render_template(self.get_template_name(), **context)
-
-    def dispatch_request(self):
-        context = {'objects': self.get_objects()}
-        return self.render_template(context)
-
-class GameView(ListView):
-
-    def get_template_name(self):
-        return
-
-    def get_objects(self):
-        pass
+        return game.platform
