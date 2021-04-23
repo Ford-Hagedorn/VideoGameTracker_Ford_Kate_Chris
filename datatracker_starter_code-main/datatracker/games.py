1ffad7fbb3ef3950199a820a261ff7ff8e1350f1
katejoyreed <katejoyreed@gmail.com>
2021-04-23 08:41:23 -0500

    Changed method for finding plaitforms and sales to include a dictionary

--- datatracker_starter_code-main/datatracker/games.py
+++ datatracker_starter_code-main/datatracker/games.py
@@ -32,8 +32,20 @@ def view_consoles():
 @bp.route('/chart')
 def chart():
     games = index()
+    after2013s = []
     platforms = []
     for game in games:
+        if game.year >= 2013:
+            after2013s.add(game)
+
+    for after2013 in after2013s:
+        if not platforms.contains(after2013.platform):
+            platforms.add(after2013.platform)
+    sales = []
+    for after2013 in after2013s:
+        if not sales.contains(after2013.globalSales):
+            sales.add(after2013.globalSales)
+
         # if game.platform is not in platforms
         # what is the easiest way to see if item is in a python list?
         # add it to platforms
@@ -42,7 +54,7 @@ def chart():
     # create dictionary using all unique console from that list
     # best way to create a dictionary with keys from a list?
 
-    for game in games:
+    dct = dict(zip(platforms, sales))
     # if game.console is in that dictionary, add its total sales to that value
 
-    return render_template('chart.html', platforms=platforms)
+    return render_template('chart.html', dct=dct)
